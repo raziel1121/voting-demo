11efa613f4c8dfe3e9b04e90ca715837d4661234
Jonathan Gostelow <jgostelow@groupon.com>
2016-06-21 16:18:24 -0700

    Change voting options to which city I should live in

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Futbol"
-optionB = "Soccer"
+optionA = "Seattle"
+optionB = "Chicago"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
